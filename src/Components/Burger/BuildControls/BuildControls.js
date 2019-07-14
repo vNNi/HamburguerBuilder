3d15433d60a9255d55e09aeda18ba1617cf0ea0d
Vinicius Komninakis <komninakis@porquenao.mobi>
2019-07-13 23:47:33 -0300

    feat: add buildcontrol component

---
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+
+const buildControls = (props) =>{
+    return (
+        <div>
+            <BuildControl label="teste"/>
+        </div>
+    )
+}
+
+export default buildControls;
