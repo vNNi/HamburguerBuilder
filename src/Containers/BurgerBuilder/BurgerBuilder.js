9f82e168b83bf192a79b89972e602f6d1979d511
Vinicius Komninakis <komninakis@porquenao.mobi>
2019-07-13 23:48:03 -0300

    feat: add buildControls wrapper

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import Burger from '../../Components/Burger/Burger';
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls';
 
 export default class BurgerBuilder extends Component {
     state = {
@@ -16,7 +17,7 @@ export default class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Build Controls</div>
+                <BuildControls/>
             </Aux>
         )
     }
