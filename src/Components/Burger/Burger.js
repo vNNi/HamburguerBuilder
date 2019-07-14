1287ab7860b0bbf4a1bc61196f119a013b986a50
Vinicius Komninakis <komninakis@porquenao.mobi>
2019-07-13 23:07:54 -0300

    feat: add burger component and burgerIngredients component

---
+++ src/Components/Burger/Burger.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import classes from "./Burger.css";
+import BurgerIngredients from "./BurgerIngredients/BurgerIngredients";
+
+const Burger = props => {
+  //essa funcao torna nosso array de ingredients flat, ou seja, deixa de ser um array de arrays para mapear, para ser um single array
+  function flattenDeep(arr1) {
+    return arr1.reduce(
+      (acc, val) =>
+        Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val),
+      []
+    );
+  }
+
+  const transformedIngredients = flattenDeep(
+    Object.keys(props.ingredients).map(keys => {
+      // pegando todas as keys do objeto e criando um array contendo a quantidade de ingredients dado na propriedade de cada objeto
+      return [...Array(props.ingredients[keys])].map((_, i) => {
+        // criando um ingrediente para cada item no array criado acima e utilizando a key que está no map anterior
+        return <BurgerIngredients key={keys + i} type={keys} />;
+      });
+    })
+  );
+  return (
+    <div className={classes.Burger}>
+      {transformedIngredients ? (
+        <>
+          <BurgerIngredients type="bread-top" />
+          {transformedIngredients}
+          <BurgerIngredients type="bread-bottom" />
+        </>
+      ) : (
+        <p>Por favor passe ingredientes !</p>
+      )}
+    </div>
+  );
+};
+
+Burger.propTypes = {
+  ingredients: PropTypes.object.isRequired
+};
+
+export default Burger;
---
+++ src/Components/Burger/BurgerIngredients/BurgerIngredients.js
@@ -0,0 +1,42 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import classes from "./BurgerIngredients.css";
+
+const BurgerIngredients = props => {
+  let ingredient = null;
+  switch (props.type) {
+    case "bread-bottom":
+      ingredient = <div className={classes.BreadBottom} />;
+      break;
+    case "bread-top":
+      ingredient = (
+        <div className={classes.BreadTop}>
+          <div className={classes.Seeds1} />
+          <div className={classes.Seeds2} />
+        </div>
+      );
+      break;
+    case "meat":
+      ingredient = <div className={classes.Meat} />;
+      break;
+    case "cheese":
+      ingredient = <div className={classes.Cheese} />;
+      break;
+    case "salad":
+      ingredient = <div className={classes.Salad} />;
+      break;
+    case "bacon":
+      ingredient = <div className={classes.Bacon} />;
+      break;
+    default:
+      ingredient = null;
+  }
+  return ingredient;
+};
+
+BurgerIngredients.propTypes = {
+  type: PropTypes.string.isRequired
+};
+
+export default BurgerIngredients;
