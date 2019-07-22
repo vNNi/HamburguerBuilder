adefea850762ee8fe7468bd21859930d27520be3
Vinicius Komninakis <komninakis@porquenao.mobi>
2019-07-21 22:11:36 -0300

    feat: add menu click handler

--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import Aux from '../../hoc/Aux';
-import Toolbar from '../../Components/Navigation/Toolbar/Toolbar';
-import classes from './Layout.css';
+import Aux from "../../hoc/Aux";
+import Toolbar from "../../Components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../Components/Navigation/SideDrawer/SideDrawer";
+import classes from "./Layout.css";
 
-export default function Layout(props) {
+export default class Layout extends Component {
+
+    state = {
+        showSideDrawer: true,
+    }
+
+    SideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    SideDrawerToggleHandler = () => {
+        this.setState((prevState)=>{
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+
+  render() {
     return (
-        <Aux>
-            <Toolbar/>
-            <main className={classes.Content}>
-                {props.children}
-            </main>
-        </Aux>
-    )
+      <Aux>
+        <Toolbar drawerToggleClicked={this.SideDrawerToggleHandler}/>
+        <SideDrawer show={this.state.showSideDrawer} closed={this.SideDrawerClosedHandler} />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
 }
