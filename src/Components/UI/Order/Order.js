da26d29303c778c3b92c4026ba963ededec52745
Vinicius Komninakis <komninakis@porquenao.mobi>
2019-07-27 18:25:50 -0300

    feat: add orders component with data from backend

--- src/Components/UI/Order/Order.js
+++ src/Components/UI/Order/Order.js
@@ -3,10 +3,31 @@ import React from 'react';
 import classes from './Order.css';
 
  const Order = (props) => {
+
+    //  const ingredients = Object.keys(props.ingredients)
+    //  .map((igKey)=>{
+    //     return [...Array(props.ingredients[igKey])].map((_,i)=>{
+    //         return <p key={igKey+i}>Ingrediente: {igKey} ({props.ingredients[igKey]}) </p>
+    //     });
+    //  });
+
+    //  other way to do same thing but clean
+    const igr = [];
+    for(let ingredientName in props.ingredients){
+        igr.push({
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        })
+    }
+    const listIgr = igr.map((igr,i)=>{
+        return <p key={igr+i}>{igr.name} ({igr.amount}) </p>
+    });
+
     return (
         <div className={classes.Order}>
-            <p>Ingredient: </p>
-            <p>Price: <strong>5,45 R$</strong></p>
+            <h4>Ingredientes:</h4>
+            {listIgr}
+            <p>Preço: <strong>{props.totalPrice.toFixed(2)} R$</strong></p>
         </div>
     )
 }
--- src/Containers/Orders/Orders.js
+++ src/Containers/Orders/Orders.js
@@ -1,16 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Order from '../../Components/UI/Order/Order';
+import Order from "../../Components/UI/Order/Order";
+import {orders as axios} from "../../axios";
+import Loading from "../../Components/UI/Loading/Loading";
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
-class Orders extends Component{
-    render() {
-        return (
-            <div>
-                <Order/>
-                <Order/>
-            </div>
-        )
-    }
+class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  };
+  componentDidMount() {
+    axios.get("/orders.json").then(res => {
+      const ordersFetched = [];
+      for (let key in res.data) {
+        ordersFetched.push({
+          ...res.data[key],
+          id: key
+        });
+      }
+      this.setState({ loading: false, orders: ordersFetched });
+    });
+  }
+  render() {
+    return (
+      <div>
+        {this.state.loading ? (
+          <Loading />
+        ) : (
+          this.state.orders.map((order) => {
+            return (
+              <Order
+                key={order.id}
+                ingredients={order.ingredients}
+                totalPrice={+order.price}
+              />
+            );
+          })
+        )}
+      </div>
+    );
+  }
 }
 
-export default Orders;
+export default withErrorHandler(Orders,axios);
